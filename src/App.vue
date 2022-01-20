9c9b454620b6ef31c24910ef5c48aeb9a8314108
Ness Lin <ness.lin@evolutivelabs.com>
2022-01-20 13:38:36 +0800

    NO2

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <div class="container">
+      <!-- search bar -->
+      <div class="search-box">
+        <input type="text" placeholder="Search...." class="search-bar" />
+      </div>
+
+      <div class="weather-wrapper">
+        <!-- location & date info -->
+        <div class="location-box">
+          <div class="location">Taichung</div>
+          <div class="date">Octorber 8th 2020</div>
+        </div>
+
+        <!-- weather info -->
+        <div class="weather-box">
+          <div class="temperature">26°C</div>
+          <div class="weather">Cloud</div>
+        </div>
+      </div>
+    </div>
   </div>
-  <router-view/>
 </template>
 
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
